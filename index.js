ae0b3e5bfb959afe5579242e4c57494c2e396ec2
kirupayini selvaraju <kirupayiniselvaraju@kirupayinis-MacBook-Pro.local>
2020-08-28 23:41:27 -0500

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-$("button").click(function(){
-  $("h1").addClass("fade_style").animate({opacity:0.5}).fadeOut("slow").fadeIn("slow");
+$(".bt1").click(function(){
+  $("#hh1").addClass("fade_style").animate({opacity:0.5}).fadeOut("slow").fadeIn("slow");
+})
+
+$(".bt2").click(function(){
+  $("#hh2").addClass("fade_style").animate({opacity:0.5}).fadeOut("slow").fadeIn("slow");
 })
