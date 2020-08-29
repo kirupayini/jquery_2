4d970c3c4eb261233e138caf16523d5582c540d8
kirupayini selvaraju <kirupayiniselvaraju@kirupayinis-MacBook-Pro.local>
2020-08-28 23:52:55 -0500

    third commit

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ $(".bt1").click(function(){
 $(".bt2").click(function(){
   $("#hh2").addClass("fade_style").animate({opacity:0.5}).fadeOut("slow").fadeIn("slow");
 })
+
+$(".bt3").click(function(){
+  $("#hh3").addClass("fade_style").animate({opacity:0.5}).fadeOut("slow").fadeIn("slow");
+})
